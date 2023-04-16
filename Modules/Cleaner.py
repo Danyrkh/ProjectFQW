d8b686ed6613b68eaacf8a479ba769b2e457c4ad
Danyrkh <danielerkh@gmail.com>
2023-04-16 12:29:40 +0700

    Initial commit

---
+++ Modules/Cleaner.py
@@ -0,0 +1,26 @@
+import pandas
+
+
+def cleaner(df, my_list):
+    for i in my_list:
+        del df[i]
+    return df
+
+
+wb = pandas.read_excel('/Users/hamster/Desktop/ВКР/Выгрузка данных.xlsx')
+wb_pfk = wb[wb['Фирма-производитель'] == 'ОБНОВЛЕНИЕ ПФК АО']
+wb_pfk['Дата'] = '1' + ' ' + wb_pfk['Месяц'].astype(str) + ' ' + wb_pfk['Год'].astype(str)
+wb_pfk = cleaner(wb_pfk,
+                 ['Регион', 'Объем (розница), упак.', 'Полное наименование', 'Бренд', 'Год', 'Месяц', 'Дозировка'])
+wb_pfk['Дата'] = pandas.to_datetime(wb_pfk['Дата']).dt.date
+wb_pfk = wb_pfk.reset_index(drop=True)
+mnn_pfk = list(set(wb_pfk['МНН']))
+
+with pandas.ExcelWriter('/Users/hamster/Desktop/ВКР/Output.xlsx') as writer:
+    for i in mnn_pfk:
+        if len(i) >= 31:
+            wb_pfk[wb_pfk['МНН'] == i].to_excel(writer, sheet_name='{}'.format(i.split(' ')[0]), index=False)
+        else:
+            wb_pfk[wb_pfk['МНН'] == i].to_excel(writer, sheet_name='{}'.format(i.partition('+')[0]), index=False)
+
+print('Operation completed successfully')
